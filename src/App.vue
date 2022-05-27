eff11355bae060f672991fcbfae0edc31e97f03c
Mikey <miketran.pl@gmail.com>
2022-05-27 16:50:01 +0100

    navbar - changed bg

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <Layout>
     <Navbar />
     <Home />
+    <Bio />
   </Layout>
 </template>
 
 <script>
 import Home from "./components/Home.vue";
 import Navbar from "./components/Navbar.vue";
+import Bio from "./components/Bio.vue";
 
 export default {
   name: "App",
   components: {
     Home,
     Navbar,
+    Bio,
   },
 };
 </script>
---
+++ src/components/Bio.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="main">
+    <div class="main__container"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Bio",
+  props: {
+    // eslint-disable-next-line vue/require-default-prop
+    msg: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.main {
+  &__container {
+  }
+}
+</style>
