46297670a321dc704d8ef5e05ee087b94b4492fa
Mikey <miketran.pl@gmail.com>
2022-06-08 01:34:32 +0100

    main - added globals and variables scss

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const { defineConfig } = require("@vue/cli-service");
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+        @import "@/assets/scss/globals.scss";
+        @import "@/assets/scss/variables.scss";
+        `,
+      },
+    },
+  },
+};
